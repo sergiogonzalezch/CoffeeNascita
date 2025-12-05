8b43cc0cb304f23c321bff62c0a2ad157006053d
SergioGlez <serchgonzalez1997@gmail.com>
2025-12-04 19:55:54 -0600

    Modifed views and fake data storage

--- apps/store/views.py
+++ apps/store/views.py
@@ -1,25 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
+
+from store.fake_data import menu_list
+
 # Create your views here.
 def index(request):
     return render(request, './index.html')
 
-def menu_list(request):
-    menu_list = [{"name": "Espresso"}, {"name": "Cappuccino"}, {"name": "Latte"}]
-    context = {
-        "menu_list": menu_list
-    }
-    return render(request, './menu_list.html', context)
+# Old function-based view
+# def menu_list(request):
+#     menu_list = [{"name": "Espresso"}, {"name": "Cappuccino"}, {"name": "Latte"}]
+#     context = {
+#         "menu_list": menu_list
+#     }
+#     return render(request, './menu_list.html', context)
 
+# New class-based view
 class MenuItemListView(TemplateView):
     template_name = 'menu_list.html'
     def get_context_data(self):
-        menu_list = [{"name": "Espresso"}, {"name": "Cappuccino"}, {"name": "Latte"}]
+        # menu_list = [{"name": "Espresso"}, {"name": "Cappuccino"}, {"name": "Latte"}]
         return {
             "menu_list": menu_list
         }
 
+# Detail view
 def detail(request, *args, **kwargs):
     print(f"ARGS: {args}")
     print(f"KWARGS: {kwargs}")
