7ed63902fade8f48ff0f388373e7194e0c7e7adb
SergioGlez <serchgonzalez1997@gmail.com>
2025-12-04 20:03:47 -0600

    Folder utils and test_scripts created

---
+++ apps/store/util/test_scripts/fake_data.py
@@ -0,0 +1,29 @@
+category_coffee_list = [{"name": "Cold Drink"}, {"name": "Hot Drink"}, {"name": "Snacks"}] 
+
+menu_list = [
+    {
+        "name": "Espresso",
+        "description": "Café concentrado de sabor intenso.",
+        "price": 2.50,
+        "have_discount": False,
+        "photo_item": None,
+        "category": category_coffee_list[1]
+    },
+    {
+        "name": "Cappuccino",
+        "description": "Café con espuma de leche cremada.",
+        "price": 3.20,
+        "have_discount": True,
+        "discount_price": 2.80,
+        "photo_item": None,
+        "category": category_coffee_list[1]
+    },
+    {
+        "name": "Latte",
+        "description": "Mezcla suave de café y leche.",
+        "price": 3.00,
+        "have_discount": False,
+        "photo_item": None,
+        "category": category_coffee_list[1]
+    }
+]
