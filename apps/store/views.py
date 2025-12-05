4782011dd18ca4e664a90decacfbb265c9939309
SergioGlez <serchgonzalez1997@gmail.com>
2025-12-04 20:03:15 -0600

    Moving Files

--- apps/store/views.py
+++ apps/store/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 
-from store.fake_data import menu_list
+# Importing fake data for testing purposes
+from store.util.test_scripts.fake_data import menu_list
+
 
 # Create your views here.
 def index(request):
-    return render(request, './index.html')
+    return render(request, "./index.html")
+
 
 # Old function-based view
 # def menu_list(request):
@@ -16,14 +19,15 @@ def index(request):
 #     }
 #     return render(request, './menu_list.html', context)
 
+
 # New class-based view
 class MenuItemListView(TemplateView):
-    template_name = 'menu_list.html'
+    template_name = "menu_list.html"
+
     def get_context_data(self):
         # menu_list = [{"name": "Espresso"}, {"name": "Cappuccino"}, {"name": "Latte"}]
-        return {
-            "menu_list": menu_list
-        }
+        return {"menu_list": menu_list}
+
 
 # Detail view
 def detail(request, *args, **kwargs):
