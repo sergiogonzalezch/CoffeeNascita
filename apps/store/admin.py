bcfd043b76b7d1a0cec670f1c74e7acca541e877
SergioGlez <serchgonzalez1997@gmail.com>
2025-12-08 20:26:48 -0600

    Modify Admin

--- apps/store/admin.py
+++ apps/store/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import MenuItem
 # Register your models here.
+
+class MenuItemAdmin(admin.ModelAdmin):
+    model = MenuItem
+    list_display = ('name', 'price', 'in_menu', 'category', 'have_discount', 'discount_price')
+    list_filter = ('in_menu', 'category', 'have_discount')
+    search_fields = ('name', 'category__name')
+
+admin.site.register(MenuItem, MenuItemAdmin)
