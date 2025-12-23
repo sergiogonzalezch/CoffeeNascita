fa527c944c9d628dc88579be79323ade5e973d7c
SergioGlez <serchgonzalez1997@gmail.com>
2025-12-22 21:33:10 -0600

    DB in postgres with AWS

--- coffee_shop/settings.py
+++ coffee_shop/settings.py
@@ -11,11 +11,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -79,8 +81,14 @@ WSGI_APPLICATION = "coffee_shop.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        # "ENGINE": "django.db.backends.sqlite3",
+        # "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env.str("DJANGO_DB_NAME"),
+        "USER": env.str("DJANGO_DB_USER"),
+        "PASSWORD": env.str("DJANGO_DB_PASSWORD"),
+        "HOST": env.str("DJANGO_DB_HOST"),
+        "PORT": env.str("DJANGO_DB_PORT"),
     }
 }
 
