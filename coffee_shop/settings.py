aa94881bfd45326ac4e736c7c65fde50604796e2
SergioGlez <serchgonzalez1997@gmail.com>
2025-12-24 13:25:36 -0600

    Fix env and settings formating

--- coffee_shop/settings.py
+++ coffee_shop/settings.py
@@ -92,7 +92,6 @@ WSGI_APPLICATION = "coffee_shop.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
-
 DATABASES = {
     # "default": {
     #     # "ENGINE": "django.db.backends.sqlite3",
